7b5c77f71589b2d131cbd4b1e26405d6a54c38c3
tajislam001 <taj.islam@gmail.com>
2025-02-05 23:18:39 +0600

    as requirement

--- lib/LiveTest_2.dart
+++ lib/LiveTest_2.dart
@@ -7,14 +7,89 @@ class Livetest2 extends StatefulWidget{
 }
 
 class _Livetest2 extends State<Livetest2>{
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context){
     return Scaffold(
+
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        title: Text("Live Test Two"),
-        centerTitle: true,
+
+        title: Text("Add Employee"),
+
       ),
+
+      body: Padding(
+    padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            Form(
+              key: _formKey,
+                child: Column(
+                  children: [
+                    TextFormField(
+
+                      decoration: InputDecoration(
+                        hintText:'Name',
+
+
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Empty Name not allow";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20,),
+                    TextFormField(
+
+                      decoration: InputDecoration(
+                        hintText:'Age',
+
+
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Empty Age not allow";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20,),
+                    TextFormField(
+
+                      decoration: InputDecoration(
+                        hintText:'Salary',
+
+
+                      ),
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return "Empty Salary not allow";
+                        }
+                        return null;
+                      },
+                    ),
+                    SizedBox(height: 20,),
+                      ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+
+                          ),
+                          onPressed: (){
+                            if(_formKey.currentState!.validate()){
+
+                            }
+
+                          },
+                          child: Text('Add Employee',style: TextStyle(fontSize: 20,),)
+                      ),
+
+                  ],
+                ))
+          ],
+        ),
+
+      )
     );
   }
 }
